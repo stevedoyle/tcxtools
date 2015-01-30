c675ef7d8560e48a49ec19c9bb62316d076eaeff
Stephen Doyle <stephendoyle75@gmail.com>
2015-01-30 23:06:27 +0000

    Adding tcxsplit.py

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # tcxtools
 Collection of utilities for processing TCX files
+
+## tcxsplit.py
+usage: tcxsplit.py [-h] tcx_files [tcx_files ...]
+
+Split a Garmin TCX file containing multiple laps into multiple TCX file, each containing 1 lap.
+
+positional arguments:
+  tcx_files   TCX files to split
+
+optional arguments:
+  -h, --help  show this help message and exit
+  
+Example output:
+./tcxsplit.py sample.tcx 
+2 laps found in sample.tcx
+Creating sample_1.tcx
+Creating sample_2.tcx
---
+++ tcxsplit.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+# Split a Garmin TCX file containing multiple laps into multiple TCX files, each containing 1 lap.
+
+import copy
+import os
+import argparse
+from lxml import objectify
+
+namespace = 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2'
+
+def remove_laps():
+    pass
+
+def count_laps(tree_root):
+    return int(tree_root.xpath('count(//ns:Lap)', namespaces={'ns': namespace}))
+
+def generate_filename_for_lap(base_filename, lap_num):
+    parts = os.path.splitext(base_filename)
+    return parts[0] + '_' + str(lap_num+1) + parts[1]
+
+def remove_laps_except(tree, lap_num):
+    laps = tree.getroot().xpath('//ns:Lap', namespaces={'ns': namespace})
+    del laps[lap_num]
+    for lap in laps:
+        lap.getparent().remove(lap)
+
+def tcxsplit(filename):
+    tree = objectify.parse(filename)
+    root = tree.getroot()
+    num_laps = count_laps(root)
+    print('%d laps found in %s' % (num_laps, filename))
+    
+    for lap_idx in range(num_laps):
+        lap_tree = copy.deepcopy(tree)
+        remove_laps_except(lap_tree, lap_idx)
+        lap_filename = generate_filename_for_lap(filename, lap_idx)
+        print('Creating ' + lap_filename)
+        lap_tree.write(lap_filename, pretty_print=True)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Split a Garmin TCX file "
+        "containing multiple laps into multiple TCX file, each containing 1 lap.")
+  
+    parser.add_argument('tcx_files', nargs='+', help="TCX files to split")
+
+    args = parser.parse_args()
+      
+    for tcx_file in args.tcx_files:
+        tcxsplit(tcx_file)
+    
