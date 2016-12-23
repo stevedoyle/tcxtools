9f418e4a128f792d55f81a4c1b7a3096a3542f6a
Stephen Doyle <stephendoyle75@gmail.com>
2016-12-23 13:06:25 +0000

    Cleaning up indentation

--- tcxsplit.py
+++ tcxsplit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-# Split a Garmin TCX file containing multiple laps into multiple TCX files, each containing 1 lap.
+# Split a Garmin TCX file containing multiple laps into multiple TCX
+# files, each containing 1 lap.
 
 import copy
 import os
@@ -9,15 +10,19 @@ from lxml import objectify
 
 namespace = 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2'
 
+
 def remove_laps():
     pass
+
 
 def count_laps(tree_root):
     return int(tree_root.xpath('count(//ns:Lap)', namespaces={'ns': namespace}))
 
+
 def generate_filename_for_lap(base_filename, lap_num):
     parts = os.path.splitext(base_filename)
-    return parts[0] + '_' + str(lap_num+1) + parts[1]
+    return parts[0] + '_' + str(lap_num + 1) + parts[1]
+
 
 def remove_laps_except(tree, lap_num):
     laps = tree.getroot().xpath('//ns:Lap', namespaces={'ns': namespace})
@@ -25,12 +30,13 @@ def remove_laps_except(tree, lap_num):
     for lap in laps:
         lap.getparent().remove(lap)
 
+
 def tcxsplit(filename):
     tree = objectify.parse(filename)
     root = tree.getroot()
     num_laps = count_laps(root)
     print('%d laps found in %s' % (num_laps, filename))
-    
+
     for lap_idx in range(num_laps):
         lap_tree = copy.deepcopy(tree)
         remove_laps_except(lap_tree, lap_idx)
@@ -40,12 +46,11 @@ def tcxsplit(filename):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Split a Garmin TCX file "
-        "containing multiple laps into multiple TCX file, each containing 1 lap.")
-  
+                                     "containing multiple laps into multiple TCX file, each containing 1 lap.")
+
     parser.add_argument('tcx_files', nargs='+', help="TCX files to split")
 
     args = parser.parse_args()
-      
+
     for tcx_file in args.tcx_files:
         tcxsplit(tcx_file)
-    
